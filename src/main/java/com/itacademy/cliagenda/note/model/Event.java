5a7aedac4126e3de8acc8749d2c6ef128729dcaa
UlisesLafuente <uliseslafuente@mailfence.com>
2026-04-09 18:40:40 +0200

    Feat: event class created

---
+++ src/main/java/com/itacademy/cliagenda/note/model/Event.java
@@ -0,0 +1,26 @@
+package com.itacademy.cliagenda.note.model;
+
+import java.time.LocalDateTime;
+
+public class Event {
+    private int Id;
+    private String name;
+    private LocalDateTime eventDate;
+    private final LocalDateTime creationDate = LocalDateTime.now();
+    private LocalDateTime lastUpdateDate;
+
+    public Event(int id, String name, LocalDateTime eventDate) {
+        Id = id;
+        this.name = name;
+        this.eventDate=eventDate;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
