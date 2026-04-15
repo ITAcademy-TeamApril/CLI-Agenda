49ff953e5dcbd252ee037429a405c5551be03946
Daniel Vilà <danivila.dev@gmail.com>
2026-04-15 08:41:21 +0200

    NoteCli addapted to eventCli style

--- src/main/java/com/itacademy/cliagenda/note/cli/NoteCli.java
+++ src/main/java/com/itacademy/cliagenda/note/cli/NoteCli.java
@@ -1,12 +1,20 @@
 package com.itacademy.cliagenda.note.cli;
 
-import com.itacademy.cliagenda.common.utils.ConsoleUtils;
+import com.itacademy.cliagenda.note.model.Note;
+import com.itacademy.cliagenda.note.service.NotesService;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class NoteCli {
 
     private final Scanner scanner = new Scanner(System.in);
+    private final NotesService service;
+
+
+    public NoteCli(NotesService service) {
+        this.service = service;
+    }
 
     public void showMenu() {
         int option = -1;
@@ -23,18 +31,60 @@ public class NoteCli {
 
             switch (option) {
                 case (1):
-                    // TODO: createNote()
+                    createNote();
                     break;
                 case (2):
-                    // TODO: listNotes()
+                    listNotes();
                     break;
                 case (3):
-                    // TODO: findNote()
+                    findNote();
                     break;
                 case (4):
-                    // TODO: deleteNote()
+                    deleteNote();
                     break;
             }
         } while (option != 0);
+    }
+
+    public void createNote() {
+        System.out.println("Introduce note body:");
+        String body = scanner.nextLine();
+        Note note = service.createNote(body);
+        System.out.println("Note created with ID: " + note.getId());
+    }
+
+    public void listNotes() {
+        List<Note> notes = service.getAllNotes();
+        if (notes.isEmpty()) {
+            System.out.println("No notes found..");
+            return;
+        }
+        for (Note note : notes)
+            System.out.println("ID: " + note.getId()
+                    + " | " + note.getBody()
+                    + " | " + note.getCreationDate());
+    }
+
+    public void findNote() {
+        System.out.println("Introduce note ID to search it:");
+        int id = scanner.nextInt();
+        scanner.nextLine(); // limpieza del fuck buffer!
+        Note note = service.findNoteById(id);
+        if (note == null) {
+            System.out.println("Note not found..");
+        } else {
+            System.out.println("ID: " + note.getId());
+            System.out.println("Body: " + note.getBody());
+            System.out.println("Created: " + note.getCreationDate());
+        }
+
+    }
+
+    public void deleteNote() {
+        System.out.println("Introduce note ID to delete it");
+        int id = scanner.nextInt();
+        scanner.nextLine(); // limpieza del mamahuevo buffer!
+        service.deleteNoteById(id);
+        System.out.println("Note with id " + id + " is correctly deleted");
     }
 }
